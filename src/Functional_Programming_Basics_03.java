17a4fbf378f43e6c3d6b35f845b951903a71e14f
GaneshAdiga <gani.adiga@gmail.com>
2025-07-28 21:24:51 +0530

    Functional programmic basics

---
+++ src/Functional_Programming_Basics_03.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Functional_Programming_Basics_03 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(2, 4,4, 3, 7, 12, 15);
+
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "microservices",
+                "aws", "docker", "azure", "Kubernettes");
+
+
+    }
+
+}
