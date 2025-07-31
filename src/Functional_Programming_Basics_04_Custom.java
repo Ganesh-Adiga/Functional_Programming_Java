edb53f475c0a873fdd8ba8194a89ba70a9384364
GaneshAdiga <gani.adiga@gmail.com>
2025-07-31 12:21:41 +0530

    examples on few stream operations

--- src/Functional_Programming_Basics_04_Custom.java
+++ src/Functional_Programming_Basics_04_Custom.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
@@ -6,11 +7,77 @@ import java.util.function.Predicate;
 
 public class Functional_Programming_Basics_04_Custom {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(2, 4, 4, 3, 7, 12, 15);
+        List<Course> courses = List.of(
+                new Course("Spring", "Framework", 99, 20000),
+                new Course("Spring Boot", "Framework", 95, 18000),
+                new Course("API", "Microservices", 97, 21000),
+                new Course("Microservices", "Microservices", 96, 22000),
+                new Course("FullStack", "Fullstack", 98, 20000));
+        Predicate<Course> reviewPredidcateGreaterThan95 = course -> course.getReviewScore()>95;
+        Predicate<Course> reviewPredidcateGreaterThan90 = course -> course.getReviewScore()>90;
+        Predicate<Course> reviewPredidcateLessThan90 = course -> course.getReviewScore()<90;
 
-        List<String> courses = List.of("Spring", "Spring Boot", "API", "microservices",
-                "aws", "docker", "azure", "Kubernettes");
+        //All, None and any mathes
+        System.out.println(courses.stream().allMatch(reviewPredidcateGreaterThan95));
+        System.out.println(courses.stream().allMatch(reviewPredidcateGreaterThan90));
+
+
+        System.out.println(courses.stream().noneMatch(reviewPredidcateGreaterThan95));
+        System.out.println(courses.stream().noneMatch(reviewPredidcateLessThan90));
+
+        System.out.println(courses.stream().anyMatch(reviewPredidcateLessThan90));
+        System.out.println(courses.stream().anyMatch(reviewPredidcateGreaterThan95));
+
+        //Comparators
+        System.out.println("************ Comparator *************");
+        Comparator<Course> noOfStudensComparator = Comparator.comparing(Course::getNoOfStudents);
+        Comparator<Course> noOfStudensComparatorRev = Comparator.comparing(Course::getNoOfStudents).reversed();
+
+        List<Course> list = courses.stream().sorted(noOfStudensComparator).toList();
+        System.out.println(list);
+
+        List<Course> listRev = courses.stream().sorted(noOfStudensComparator).toList();
+        System.out.println(listRev);
+
+        Comparator<Course> noOfStudensAndReviewComparator = Comparator.comparing(Course::getNoOfStudents).thenComparing(Course::getReviewScore);
+        System.out.println(courses.stream().sorted(noOfStudensAndReviewComparator).toList());
+
+        //limit, skip
+        System.out.println("************ skip limit takewhile dropwhile  *************");
+
+        System.out.println(courses.stream().limit(2).toList());
+
+        System.out.println(courses.stream().skip(1).toList());
+
+        System.out.println(courses);
+
+        System.out.println(courses.stream().takeWhile(course -> course.getReviewScore()>=99).toList());
+
+        System.out.println(courses.stream().dropWhile(course -> course.getReviewScore()>95).toList());
+
+        //max min optional firnd firstfirst and find any
+        System.out.println("************ skip limit takewhile dropwhile  *************");
+
+        System.out.println(courses.stream().max(noOfStudensAndReviewComparator));
+        System.out.println(courses.stream().
+                min(noOfStudensAndReviewComparator)
+                .orElse(new Course("FullStack", "Fullstack", 98, 20000)));
+
+
+        System.out.println(courses.stream().filter(reviewPredidcateLessThan90).min(noOfStudensAndReviewComparator));
+        System.out.println(courses.stream().filter(reviewPredidcateLessThan90)
+                .min(noOfStudensAndReviewComparator).orElse(new Course("FullStack", "Fullstack", 98, 20000)));
+
+        System.out.println(courses.stream()
+                .filter(reviewPredidcateGreaterThan90)
+                .findFirst());
+
+        System.out.println(courses.stream()
+                .filter(reviewPredidcateGreaterThan90)
+                .findAny());
+
     }
+
 
 }
 
